4b344091370263a98630623fdb6a083b680a9403
Zachary Hirsch <zhirsch@umich.edu>
2017-03-09 13:27:08 -0500

    Add a script to download the manifest database.

---
+++ update_manifest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import json
+import os
+import sys
+import tempfile
+import urllib2
+import zipfile
+
+# Get the manifest urls.
+req = urllib2.Request(
+    "https://www.bungie.net//platform/Destiny/Manifest/",
+    headers={'X-API-Key': sys.argv[1]},
+)
+resp = json.loads(urllib2.urlopen(req).read())
+if resp['ErrorCode'] != 1:
+    raise Exception("error: %s", resp)
+
+with tempfile.TemporaryFile() as tf:
+    # Download the zipped database.
+    path = resp['Response']['mobileWorldContentPaths']['en']
+    resp = urllib2.urlopen("https://www.bungie.net%s" %  path)
+    while True:
+        chunk = resp.read(16 << 10)
+        if not chunk:
+            break
+        tf.write(chunk)
+
+    # Unzip the database to the current directory.
+    tf.seek(0)
+    with zipfile.ZipFile(tf, 'r') as f:
+        f.extractall()
